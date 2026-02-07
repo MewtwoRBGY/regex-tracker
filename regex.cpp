a459dcb8b0bfef4833b4692787263268a6382a56
Joseph Racca <jracca001@student.franciscan.edu>
2026-02-07 16:25:27 -0500

    Changed to /w to /s

--- regex.cpp
+++ regex.cpp
@@ -14,13 +14,13 @@ int main() {
     string address = "";
     regex house("");
     string BirthDate("");
-    regex bday("[/d]{4}(/w|-)((1[0-2])|(0[1-9]))(/w|-)((0[0-9])|(1[0-9])|(2[0-9])|(3[0-1]))");
+    regex bday("[/d]{4}(/s|-)((1[0-2])|(0[1-9]))(/s|-)((0[0-9])|(1[0-9])|(2[0-9])|(3[0-1]))");
     string SSN = "";
     regex tracker("[0-9]{3}(-| )[0-9]{2}(-| )[0-9]{4}");
     string FoneNum = "";
     regex phone("^[0-9]{3}-[0-9]{3}-[0-9]{4}$");
     string CardNum = "";
-    regex money("[/d]{4}(/w|-)?[/d]{4}(/w|-)?[/d]{4}(/w|-)?[/d]{3}(/w|-)?[/d]?");
+    regex money("[/d]{4}(/s|-)?[/d]{4}(/s|-)?[/d]{4}(/s|-)?[/d]{3}(/s|-)?[/d]?");
     while(regex_match(name, nombre)) {
         cout << "put in your first, middle, and last name" << endl;
         cin >> name;
