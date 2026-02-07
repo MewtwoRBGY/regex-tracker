6ae7bf82a3a797055c705a4b5761be68534b8e37
Joseph Racca <jracca001@student.franciscan.edu>
2026-02-07 16:44:37 -0500

    Made loops do whiles

--- regex.cpp
+++ regex.cpp
@@ -21,48 +21,48 @@ int main() {
     regex phone("^[0-9]{3}-[0-9]{3}-[0-9]{4}$");
     string CardNum = "";
     regex money("[/d]{4}(/s|-)?[/d]{4}(/s|-)?[/d]{4}(/s|-)?[/d]{3}(/s|-)?[/d]?");
-    while(regex_match(name, nombre)) {
+    do  {
         cout << "put in your first, middle, and last name" << endl;
         cin >> name;
         if(regex_match(name, nombre)) {
             cout << "Incorrect format. Try again"<<endl;
         }
-    }
-    while(regex_match(address, house)) {
+    } while(regex_match(name, nombre));
+    /*do {
         cout << "put in your street address" << endl;
         cin >> address;
         if(regex_match(address, house)) {
             cout << "Incorrect format. Try again"<<endl;
         }
-    }
-    while(regex_match(BirthDate, bday)) {
+    } while(regex_match(address, house));*/
+    do {
         cout << "put in your date of birth" << endl;
         cin >> BirthDate;
         if(regex_match(BirthDate, bday)) {
             cout << "Incorrect format. Try again"<<endl;
         }
-    }
-    while(regex_match(SSN, tracker)) {
+    } while(regex_match(BirthDate, bday));
+    do {
         cout << "put in your SSN" << endl;
         cin >> SSN;
         if(regex_match(SSN, tracker)) {
             cout << "Incorrect format. Try again"<<endl;
         }
-    }
-    while(regex_match(FoneNum, phone)) {
+    } while(regex_match(SSN, tracker));
+    do  {
         cout << "put in your phone number" << endl;
         cin >> FoneNum;
         if(regex_match(FoneNum, phone)) {
             cout << "Incorrect format. Try again"<<endl;
         }
-    }
-    while(regex_match(CardNum, money)) {
+    } while(regex_match(FoneNum, phone));
+    do {
         cout << "put in your credit card number" << endl;
         cin >> CardNum;
         if(regex_match(CardNum, money)) {
             cout << "Incorrect format. Try again"<<endl;
         }
-    }
+    } while(regex_match(CardNum, money));
 
     return 0;
 }
