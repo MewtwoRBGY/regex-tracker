f9e70aaedfe408a3e4465f2d024cfc78836b8518
Shazookie <seamusmsommers@gmail.com>
2026-02-07 15:41:22 -0500

    Added basic prompts for the info

--- regex.cpp
+++ regex.cpp
@@ -7,5 +7,28 @@ using namespace std;
 
 int main() {
     
+    string name;
+    string address;
+    string SSN;
+    string FoneNum;
+    string CardNum;
+    
+        cout << "put in your first middle and last name" << endl;
+        cin >> name;
+
+        cout << "put in your street address" << endl;
+        cin >> address;
+        
+        cout << "put in your SSN" << endl;
+        cin >> SSN;
+
+        cout << "put in your phone number" << endl;
+        cin >> FoneNum;
+
+        cout << "put in your credit card number" << endl;
+        cin >> CardNum;
+
+        
+
     return 0;
 }
